7f5e026a423e8e3fc2a5fddf26a3610840d54e23
Corentin Chrisment <tintinch59@gmail.com>
2022-03-24 20:16:23 +0100

    - created Barbou files

---
+++ Source/UnrealFPTeam01/BarbouTower.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TowerBase.h"
+#include "BarbouTower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALFPTEAM01_API ABarbouTower : public ATowerBase
+{
+	GENERATED_BODY()
+	
+};
